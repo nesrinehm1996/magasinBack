673351614fb23571a5929ff337972f30caff3307
NourehaneGharbi <61804846+NourehaneGharbi@users.noreply.github.com>
2022-10-16 23:39:10 +0200

    Update ReglementServiceImpl.java

--- src/main/java/com/esprit/examen/services/ReglementServiceImpl.java
+++ src/main/java/com/esprit/examen/services/ReglementServiceImpl.java
@@ -17,6 +17,7 @@ public class ReglementServiceImpl implements IReglementService {
 	FactureRepository factureRepository;
 	@Autowired
 	ReglementRepository reglementRepository;
+	
 	@Override
 	public List<Reglement> retrieveAllReglements() {
 		return (List<Reglement>) reglementRepository.findAll();
@@ -30,19 +31,12 @@ public class ReglementServiceImpl implements IReglementService {
 
 	@Override
 	public Reglement retrieveReglement(Long id) {
-		Reglement reglement = reglementRepository.findById(id).orElse(null);
-		
-		return reglement;
+		return reglementRepository.findById(id).orElse(null);
 	}
 
 	@Override
 	public List<Reglement> retrieveReglementByFacture(Long idFacture) {
-		List<Reglement> reglements= reglementRepository.retrieveReglementByFacture(idFacture);
-		return reglements;
-		
-//		ou bien(Sans JPQL)
-//		Facture f= factureRepository.findById(idFacture).get();
-//		return (List<Reglement>) f.getReglements();
+		return reglementRepository.retrieveReglementByFacture(idFacture);
 	}
 
 	@Override
