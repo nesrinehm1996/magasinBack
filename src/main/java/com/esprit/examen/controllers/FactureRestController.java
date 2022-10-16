46310e7d7154f6d19bf9ff225d4f53941f2f6338
NourehaneGharbi <61804846+NourehaneGharbi@users.noreply.github.com>
2022-10-16 16:40:35 +0200

    Update FactureRestController.java

--- src/main/java/com/esprit/examen/controllers/FactureRestController.java
+++ src/main/java/com/esprit/examen/controllers/FactureRestController.java
@@ -15,7 +15,7 @@ import io.swagger.annotations.Api;
 @RestController
 @Api(tags = "Gestion des factures")
 @RequestMapping("/facture")
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 public class FactureRestController {
 
     @Autowired
