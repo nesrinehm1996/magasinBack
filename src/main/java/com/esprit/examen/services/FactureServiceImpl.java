b1e89ec1b5097f7d8fc1bdfef4d9b3690c7c5abe
NourehaneGharbi <61804846+NourehaneGharbi@users.noreply.github.com>
2022-10-16 14:01:20 +0200

    Update FactureServiceImpl.java

--- src/main/java/com/esprit/examen/services/FactureServiceImpl.java
+++ src/main/java/com/esprit/examen/services/FactureServiceImpl.java
@@ -59,7 +59,10 @@ public class FactureServiceImpl implements IFactureService {
 		float montantRemise = 0;
 		for (DetailFacture detail : detailsFacture) {
 			//Récuperer le produit 
-			Produit produit = produitRepository.findById(detail.getProduit().getIdProduit()).get();
+			Produit produit = null;
+			if(produit != null){
+				produit = produitRepository.findById(detail.getProduit().getIdProduit()).get();
+			}
 			//Calculer le montant total pour chaque détail Facture
 			float prixTotalDetail = detail.getQteCommandee() * produit.getPrix();
 			//Calculer le montant remise pour chaque détail Facture
