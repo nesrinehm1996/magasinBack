3d32daecdbe9ccf1d2206e84faade69de9356057
NourehaneGharbi <61804846+NourehaneGharbi@users.noreply.github.com>
2022-10-16 16:33:50 +0200

    Update DetailFacture.java

--- src/main/java/com/esprit/examen/entities/DetailFacture.java
+++ src/main/java/com/esprit/examen/entities/DetailFacture.java
@@ -1,7 +1,6 @@
 package com.esprit.examen.entities;
 
 import java.io.Serializable;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
