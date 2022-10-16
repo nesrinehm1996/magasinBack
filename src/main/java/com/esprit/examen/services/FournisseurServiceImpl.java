5017c42886b4cc281f61dc4c073f56bd27636af2
NourehaneGharbi <61804846+NourehaneGharbi@users.noreply.github.com>
2022-10-16 14:12:34 +0200

    Update FournisseurServiceImpl.java

--- src/main/java/com/esprit/examen/services/FournisseurServiceImpl.java
+++ src/main/java/com/esprit/examen/services/FournisseurServiceImpl.java
@@ -76,7 +76,7 @@ public class FournisseurServiceImpl implements IFournisseurService {
 
 	@Override
 	public void assignSecteurActiviteToFournisseur(Long idSecteurActivite, Long idFournisseur) {
-		Fournisseur fournisseur = fournisseurRepository.findById(idFournisseur).orElse(null);
+		Fournisseur fournisseur = fournisseurRepository.findById(idFournisseur).orElse(new Fournisseur());
 		SecteurActivite secteurActivite = secteurActiviteRepository.findById(idSecteurActivite).orElse(null);
         fournisseur.getSecteurActivites().add(secteurActivite);
         fournisseurRepository.save(fournisseur);
