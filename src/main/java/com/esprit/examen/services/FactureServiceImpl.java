95ea3059a272448538b6a82151583f37c90d8985
NourehaneGharbi <61804846+NourehaneGharbi@users.noreply.github.com>
2022-10-16 23:50:03 +0200

    Update FactureServiceImpl.java

--- src/main/java/com/esprit/examen/services/FactureServiceImpl.java
+++ src/main/java/com/esprit/examen/services/FactureServiceImpl.java
@@ -114,7 +114,7 @@ public class FactureServiceImpl implements IFactureService {
 	public float pourcentageRecouvrement(Date startDate, Date endDate) {
 		float totalFacturesEntreDeuxDates = factureRepository.getTotalFacturesEntreDeuxDates(startDate,endDate);
 		float totalRecouvrementEntreDeuxDates =reglementService.getChiffreAffaireEntreDeuxDate(startDate,endDate);
-		float pourcentage = (totalRecouvrementEntreDeuxDates/totalFacturesEntreDeuxDates)*100;
+		float pourcentage = (totalRecouvrementEntreDeuxDates/totalFacturesEntreDeuxDates);
 		return pourcentage;
 	}
 	
